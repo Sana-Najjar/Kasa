c487bcb2c8b80f9643e29efcf29e5307db29c7a8
Sana-Najjar <najjarsana@hotmail.fr>
2024-04-06 11:59:53 +0200

    correction de navigation entre lien

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Card.scss";
+import { useNavigate } from "react-router-dom";
 
 const Card = ({ id, title, cover }) => {
+  const navigate = useNavigate()
+  const goToLogementPage = (id) => {
+    navigate(`/logements/${id}`)
+  }
   return (
-    <li key={id} className="logements">
-      <a href={"/logements/" + id}>
-        <figure className="logements_figure">
+    <li className="logements" onClick={()=> goToLogementPage(id)}>
+       <figure className="logements_figure">
           <img src={cover} alt={title} className="logements_figure_cover" />
           <figcaption className="logements_figure_figcaption">
             <h2 className="logements_figure_figcaption_title">{title}</h2>
           </figcaption>
         </figure>
-      </a>
+     
     </li>
   );
 };
